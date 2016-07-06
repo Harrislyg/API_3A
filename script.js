dfae631a1d4b441a07648806af814e2141f51477
HarrisLeow <leow_harris@hotmail.sg>
2016-07-06 22:49:23 +0800

    Added Spotify and Forex API

--- script.js
+++ script.js
@@ -19,17 +19,59 @@ $(document).ready(function () {
   // });
 
 $('#gifForm').on('submit', function (event) {
-  console.log("Hello");
   event.preventDefault();
   $('#main *').remove();
   var data = $(this).serialize();
+  console.log(data);
   $.getJSON('http://api.giphy.com/v1/gifs/search?' + data, function (result) {
     $.each(result.data, function (index, obj) {
       console.log(obj);
-      $('#main').append('<img src="' + obj.images.original.url + '"/>');
+      $('#main').append('<img src="' + obj.images.original.url + '"class="img-responsive img-thumbnail" alt="Responsive image" id="imageBox"/>');
     });
   });
 });
+
+$('#moneyForm').on('submit', function (event) {
+  event.preventDefault();
+  $('#main *').remove();
+  var data = $(this).serialize().replace('&symbols2=', ',');
+  console.log(data);
+  $.getJSON('http://api.fixer.io/latest?' + data, function (result) {
+      console.log(result.rates);
+      $.each(result.rates, function(currency, rate) {
+        $('#main').append('<div class="panel panel-info"><div class="panel-heading">' + currency + '</div><div class="panel-body">' + rate + '</div></div>');
+
+        // $('#main').append('<p>' + currency + ': ' + rate + '</p>');
+      });
+    });
+  });
+
+  $('#spotifyForm').on('submit', function (event) {
+    event.preventDefault();
+    $('#main *').remove();
+    var query = $('#spotify').val();
+    // console.log(query);
+    $.ajax({
+          url: 'https://api.spotify.com/v1/search',
+          data: {
+              q: query,
+              type: 'album'
+          },
+          success: function (response) {
+            $.each(response.albums.items, function(index, item) {
+
+                $('#main').append('<img src="' + item.images[1].url + '"class="img-responsive img-circle" alt="Responsive image" id="imageBox"/>');
+                console.log(response.albums.items);
+            });
+        }
+    });
+    });
+
+
+
+
+
+
 
 
 });
